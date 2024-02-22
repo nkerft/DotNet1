ad7b697fcd689bf81c5cfa86291c32f5a7975f73
Nikolay Ermakov <nk.er.ft@gmail.com>
2024-02-22 12:03:13 +0100

    lesson1:Task2:Updates

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,16 +1,16 @@
 ﻿int i5 = 5;
 int i6 = 6;
 
-int sum = i5+i6;
+int sum = 5+i6;
 Console.WriteLine(sum);
 
 long l1 = 10;
 double d1 = 5.65;
 
-long sum2 = l1 + i5;
+long sum2 = l1 + 5;
 double sum3 = d1 + i6;
 double delenie = i6 / d1;
-long umnozhenie = sum2 * i5;
+long umnozhenie = sum2 * 5;
 
 Console.WriteLine(sum2);
 Console.WriteLine(sum3);
