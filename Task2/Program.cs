8be3717e4660dd8e4d70f501f2323b2ecc00c033
Nikolay Ermakov <nk.er.ft@gmail.com>
2024-02-22 12:01:20 +0100

    lesson1:Task2:Creation

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿int i5 = 5;
+int i6 = 6;
+
+int sum = i5+i6;
+Console.WriteLine(sum);
+
+long l1 = 10;
+double d1 = 5.65;
+
+long sum2 = l1 + i5;
+double sum3 = d1 + i6;
+double delenie = i6 / d1;
+long umnozhenie = sum2 * i5;
+
+Console.WriteLine(sum2);
+Console.WriteLine(sum3);
+Console.WriteLine(delenie);
+Console.WriteLine(umnozhenie);
 
 
